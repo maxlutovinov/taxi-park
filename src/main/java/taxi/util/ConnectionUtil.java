5faafe365f6bd490de4b8d33664ed084f8ad47f2
Max Lutovinov <90464472+maxlutovinov@users.noreply.github.com>
2022-08-27 01:24:10 +0300

    Add dbConfig.properties and modify ConnectionUtil.java to use dbConfig

--- src/main/java/taxi/util/ConnectionUtil.java
+++ src/main/java/taxi/util/ConnectionUtil.java
@@ -1,32 +1,31 @@
 package taxi.util;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL = "YOUR DATABASE URL";
-    private static final String USERNAME = "YOUR USERNAME";
-    private static final String PASSWORD = "YOUR PASSWORD";
-    private static final String JDBC_DRIVER = "YOUR DRIVER";
-
-    static {
-        try {
-            Class.forName(JDBC_DRIVER);
+    public static Connection getConnection() {
+        try (var input = ConnectionUtil.class.getClassLoader()
+                .getResourceAsStream("dbConfig.properties")) {
+            if (input == null) {
+                throw new RuntimeException("Can't find dbConfig.properties");
+            }
+            Properties dbProperties = new Properties();
+            dbProperties.load(input);
+            Class.forName(dbProperties.getProperty("jdbc.driver"));
+            return DriverManager.getConnection(
+                    dbProperties.getProperty("db.url"),
+                    dbProperties.getProperty("db.username"),
+                    dbProperties.getProperty("db.password"));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read dbConfig.properties", e);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("Can't find SQL Driver", e);
-        }
-    }
-
-    public static Connection getConnection() {
-        Properties dbProperties = new Properties();
-        dbProperties.setProperty("user", USERNAME);
-        dbProperties.setProperty("password", PASSWORD);
-        try {
-            return DriverManager.getConnection(URL, dbProperties);
         } catch (SQLException e) {
-            throw new RuntimeException("Can't create connection to DB ", e);
+            throw new RuntimeException("Can't create connection to DB", e);
         }
     }
 }
